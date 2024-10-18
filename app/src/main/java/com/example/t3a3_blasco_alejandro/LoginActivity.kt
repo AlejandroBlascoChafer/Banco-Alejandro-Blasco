0cbb7d4b8e2b68ccd6eeda46919156608308ab14
Alejandro <alexblascochafer16@gmail.com>
2024-10-18 16:28:06 +0200

    T3A3 Acabada

--- app/src/main/java/com/example/t3a3_blasco_alejandro/LoginActivity.kt
+++ app/src/main/java/com/example/t3a3_blasco_alejandro/LoginActivity.kt
@@ -1,16 +1,61 @@
 package com.example.t3a3_blasco_alejandro
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.t3a3_blasco_alejandro.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSalir.setOnClickListener{
+            finishAffinity()
+        }
+
+        binding.btnEntrar.setOnClickListener{
+
+            val usuario = binding.etUsuario.text.toString()
+
+            val contrasena = binding.etPassword.text.toString()
+
+            if (usuario.isEmpty() || contrasena.isEmpty()) {
+
+                // Mostrar un mensaje de error si algún campo está vacío
+                Toast.makeText(this, "Por favor, completa todos los campos.", Toast.LENGTH_SHORT).show()
+
+            } else {
+
+                val intent = Intent(this, MainActivity::class.java).apply {
+                    putExtra("DNI", usuario)
+                }
+
+                startActivity(intent)
+            }
+
+
+
+
+
+
+
+
+        }
+
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
